ada495f0d68bc91ff1e7d93140adfab67f2c5591
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2024-12-12 11:06:27 -0600

    remove button from header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,12 +3,9 @@
 import React from "react";
 import Image from "next/image";
 
-import Button from "../Button";
-
 import styles from "./styles.module.css";
 
 import Rocket from "@/assets/rocket.svg";
-import Plus from "@/assets/icons/plus-icon.svg";
 
 export default function Header() {
   return (
@@ -17,14 +14,6 @@ export default function Header() {
       <div className={`font-extrabold ml-3 ${styles.title}`}>
         <span className="text-[#4EA8DE]">Todo</span>
         <span className="ml-2 text-[#5E60CE]">App</span>
-      </div>
-      <div className="absolute top-[175px] w-[100%] pl-[25%] pr-[25%]">
-        <Button height="50px" onClick={() => {}}>
-          <div className="flex items-center justify-center">
-            <span className="mr-1">Create Task</span>
-            <Image src={Plus.src} width={15} height={15} alt="plus" />
-          </div>
-        </Button>
       </div>
     </div>
   );
